9d5670ce35d403caeebd951c0602ca5218ea44c2
星辰 <difierline@yeah.net>
2026-01-07 04:19:00 +0800

    Add files via upload

--- luamodule/jni/new/Android.mk
+++ luamodule/jni/new/Android.mk
@@ -1,12 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := LXCLuaCore
 LOCAL_SRC_FILES := libs/arm64-v8a/libLXCLuaCore.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libs/lua $(LOCAL_PATH)/libs/luajava
 include $(PREBUILT_SHARED_LIBRARY)
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE     := mysample
@@ -15,11 +13,10 @@ LOCAL_C_INCLUDES := $(LXCLuaCore_EXPORT_C_INCLUDES)
 
 
 LOCAL_CFLAGS := -std=c17 -O0 -fomit-frame-pointer -ffunction-sections -fdata-sections
-LOCAL_CFLAGS += -g0 -DNDEBUG -fno-exceptions
+LOCAL_CFLAGS += -g0 -DNDEBUG -fno-exceptions -fvisibility=default
 
 
 LOCAL_LDFLAGS := -Wl,--gc-sections
-
 
 LOCAL_SHARED_LIBRARIES := LXCLuaCore
 LOCAL_LDLIBS += -llog -ldl -lz
